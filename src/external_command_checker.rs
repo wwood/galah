936c3372679f0c220879267b2b19d1b8b593d107
Ben J Woodcroft <donttrustben@gmail.com>
2020-02-18 09:51:30 +1000

    external_command_checker: Add overall method.

--- src/external_command_checker.rs
+++ src/external_command_checker.rs
@@ -1,5 +1,10 @@
 use bird_tool_utils::external_command_checker::*;
 
+pub fn check_for_dependencies() {
+    check_for_fastani();
+    check_for_dashing();
+}
+
 pub fn check_for_fastani() {
     self::check_for_external_command_presence("fastaANI", "which fastANI");
     self::default_version_check("fastANI","1.3", false, None);
