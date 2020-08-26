bf3fde516af5199dd464399675d658f757125c40
Ben Woodcroft <donttrustben@gmail.com>
2020-08-26 16:10:02 +1000

    docs: Rename doc/ -> docs/.

--- release.sh
+++ release.sh
@@ -30,9 +30,9 @@ cd ..
 echo "Building HTML versions of man pages .."
 for SUBCOMMAND in cluster
 do
-    cargo run -- $SUBCOMMAND --full-help-roff |pandoc - -t markdown -f man |sed 's/\\\[/[/g; s/\\\]/]/g' |cat <(sed s/SUBCOMMAND/$SUBCOMMAND/ prelude) - >doc/galah-$SUBCOMMAND.Rmd
-    echo "library(prettydoc); setwd('doc'); rmarkdown::render('galah-$SUBCOMMAND.Rmd','prettydoc::html_pretty','galah-$SUBCOMMAND.html')" |R --no-save </dev/stdin
-    rm doc/galah-$SUBCOMMAND.Rmd
+    cargo run -- $SUBCOMMAND --full-help-roff |pandoc - -t markdown -f man |sed 's/\\\[/[/g; s/\\\]/]/g' |cat <(sed s/SUBCOMMAND/$SUBCOMMAND/ prelude) - >docs/galah-$SUBCOMMAND.Rmd
+    echo "library(prettydoc); setwd('docs'); rmarkdown::render('galah-$SUBCOMMAND.Rmd','prettydoc::html_pretty','galah-$SUBCOMMAND.html')" |R --no-save </dev/stdin
+    rm docs/galah-$SUBCOMMAND.Rmd
 done
 
 echo "Now make sure git is up to date, and run cargo publish"
