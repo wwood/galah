5a218718c14129b73cca7384f7396aa7b69d8ffc
Ben Woodcroft <donttrustben@gmail.com>
2023-11-14 07:53:01 +1000

    skani: Performance improvements. #36.

--- src/skani.rs
+++ src/skani.rs
@@ -30,32 +30,44 @@ fn precluster_skani(
     threshold: f32,
     min_aligned_threshold: f32,
 ) -> SortedPairGenomeDistanceCache {
+    let (command_params, sketch_params) = default_params(Mode::Dist, min_aligned_threshold);
+
+    debug!("Sketching genomes with skani ..");
+    let fasta_strings = genome_fasta_paths
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    let sketches = &file_io::fastx_to_sketches(&fasta_strings, &sketch_params, true);
+
     // Right now implemented by parallel collection into a queue, and then
     // reprocessed into a BTreeMap. Could potentially be made more efficient by
     // direct collection into a concurrent BTreeMap, but eh for now.
     let queue = ConcurrentQueue::unbounded();
-    genome_fasta_paths
-        .par_iter()
-        .enumerate()
-        .for_each(|(i, fasta1)| {
-            (genome_fasta_paths[(i + 1)..genome_fasta_paths.len()])
-                .par_iter()
-                .enumerate()
-                .for_each(|(j, fasta2)| {
-                    let genome_index2 = i + j + 1;
-                    let ani = calculate_skani(fasta1, fasta2, min_aligned_threshold);
-                    if ani >= threshold {
-                        queue
-                            .push((i, genome_index2, ani))
-                            .expect("Failed to push to queue during preclustering");
-                    }
-                });
-        });
+
+    info!("Calculating ANI from skani sketches ..");
+    sketches.par_iter().enumerate().for_each(|(i, ref_sketch)| {
+        (sketches[(i + 1)..sketches.len()])
+            .par_iter()
+            .enumerate()
+            .for_each(|(j, query_sketch)| {
+                let genome_index2 = i + j + 1;
+                let map_params = chain::map_params_from_sketch(ref_sketch, false, &command_params);
+                let ani_result = chain::chain_seeds(ref_sketch, query_sketch, map_params);
+                let ani = ani_result.ani * 100.;
+                if ani >= threshold {
+                    queue
+                        .push((i, genome_index2, ani))
+                        .expect("Failed to push to queue during preclustering");
+                }
+            });
+    });
+    debug!("Converting ANI results into sparse cache ..");
 
     let mut to_return = SortedPairGenomeDistanceCache::new();
     while let Ok((i, j, ani)) = queue.pop() {
         to_return.insert((i, j), Some(ani));
     }
+    debug!("Finished skani.");
 
     to_return
 }
