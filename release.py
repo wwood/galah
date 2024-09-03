7ed5366100621915a1d2d3e81375d8b8776702bd
Ben Woodcroft <donttrustben@gmail.com>
2024-09-03 15:54:36 +1000

    release.py: Update instructions.

--- release.py
+++ release.py
@@ -40,5 +40,5 @@ if __name__ == "__main__":
     extern.run('if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then exit 1; fi')
 
     extern.run('git tag v{}'.format(version))
-    print("Now run 'cargo publish' and then 'git push && git push --tags' and GitHub actions will build and upload to PyPI".format(version))
-    print("Then make a release, adding changelog info, so Zenodo picks it up")
+    print("Now run 'cargo publish && git push && git push --tags'".format(version))
+    print("Then make a release, adding changelog info, so Zenodo picks it up, uploading the musl binary to the release too")
